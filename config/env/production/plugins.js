4802a14cc05030a92015d430c7efeefc5e3f021f
joelebukatobi <joelebuka@gmail.com>
2022-06-07 18:17:36 +0100

    Added Email Providers and Cloudinary

---
+++ config/env/production/plugins.js
@@ -0,0 +1,32 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
+
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "joelebuka@gmail.com",
+        defaultReplyTo: "joelebuka@gmail.com",
+      },
+    },
+  },
+});
